54bd313c2020d694279cfc8b97778c8e49190343
harshdixit3 <harsh.dixit@3gmail.com>
2024-10-11 00:43:57 +0530

    first commit

---
+++ ping-pong-game/application/dist/utils.js
@@ -0,0 +1,2 @@
+const random = (min, max) => Math.floor(min - max + 1) * Math.random();
+export { random };
---
+++ ping-pong-game/application/src/utils.ts
@@ -0,0 +1,5 @@
+const random = ( min : number , max : number ) => 
+   Math.floor (min - max + 1 ) * Math.random();
+
+export { random };
+
